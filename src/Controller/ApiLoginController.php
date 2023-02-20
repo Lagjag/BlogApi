212ce40e45fb8a1e8d6071721fd18449e9727653
Lagjag <lualamillo@gmail.com>
2023-02-20 19:28:21 +0100

    Login duplicated oa

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -13,27 +13,6 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 #[Route('/api', name: 'api_')]
 class ApiLoginController extends AbstractController
 {
-    /**
-    * Login for users
-    *
-    * @OA\Response(
-    *     response=200,
-    *     description="Login successfull",
-    *     @OA\JsonContent(
-    *        type="string"
-    *     )
-    * )
-    * @OA\Parameter(
-    *     in="path",
-    *     name="body",
-    *     @OA\JsonContent(
-    *         type="object",
-    *         @OA\Property(property="username", type="string"),
-    *         @OA\Property(property="password", type="string"),
-    *     ),
-    * )
-    * 
-    */
     #[Route('/login_check_api', name: 'login_check_api', methods: ['POST'])]
     public function index(#[CurrentUser] ?User $user): Response
     {
